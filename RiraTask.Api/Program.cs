8b491dcddafd3d56a4adb4aedabc46473fd347ec
Reza Ghorbani <rg@rezaghorbani.dev>
2024-12-02 16:56:09 +0330

    [Feature] Add MongoDb configs

--- RiraTask.Api/Program.cs
+++ RiraTask.Api/Program.cs
@@ -1,4 +1,12 @@
-using RiraTask.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using RiraTask.Api.AutoMapper;
+using RiraTask.Api.Controllers;
+using RiraTask.Api.Middlewares;
+using RiraTask.Core.Services;
+using RiraTask.Core.Services.Interfaces;
+using RiraTask.Infrastructure.Data;
+using RiraTask.Infrastructure.Repositories;
+using RiraTask.Infrastructure.Repositories.Interfaces;
 
 namespace RiraTask.Api;
 
@@ -8,16 +16,31 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddGrpc();
+        ConfigureMongoDb(builder);
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        app.MapGrpcService<GreeterService>();
-        app.MapGet("/",            () =>
-                "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+        ConfigurePipelines(app);
+
+        app.MapGet("/", () =>
+            "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
         app.Run();
+    }
+
+    private static void ConfigurePipelines(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        app.MapGrpcService<GreeterController>();
+    }
+
+    private static void ConfigureMongoDb(WebApplicationBuilder builder)
+    {
+        // Configure MongoDB settings
+        builder.Services.Configure<MongoDbSettings>(
+            builder.Configuration.GetSection(nameof(MongoDbSettings)));
+
+        // Register MongoDB context as a singleton
+        builder.Services.AddSingleton<MongoDbContext>();
     }
 }
